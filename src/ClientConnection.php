cac29c84f63b6701ddf03e2f0fadac4cdad2b77e
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-10-10 09:23:35 +0200

    Add ClientConnection->remote_addr

--- src/ClientConnection.php
+++ src/ClientConnection.php
@@ -3,7 +3,12 @@ namespace paws;
 /** A server-to-client websocket connection. */
 class ClientConnection extends Connection
 {
-	public $stream;
+	/**
+	 * The remote's address, e.g. "127.0.0.1:50420". This value persists after the stream is closed.
+	 *
+	 * @var string $remote_addr
+	 */
+	public $remote_addr;
 	public $path;
 	public $host;
 	public $next_ping;
@@ -12,6 +17,7 @@ class ClientConnection extends Connection
 	function __construct($stream, string $path = "", string $host = "")
 	{
 		$this->stream = $stream;
+		$this->remote_addr = stream_socket_get_name($stream, true);
 		$this->path = $path;
 		$this->host = $host;
 		$this->next_ping = microtime(true) + 30;
